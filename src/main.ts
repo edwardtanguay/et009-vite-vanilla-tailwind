8a8ba8f6a3dd1714ca07aea96f1e27f614904b76
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-17 17:56:21 +0200

    finish2

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   <h1 class="text-3xl underline text-red-600">Tailwind test</h1>
+  <p class="text-green-500">sdfksd</p>
   </div>
 `
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [],
   server: {
     hmr: {
       overlay: false
